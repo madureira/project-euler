940c68ed3605b8e0dc827478498a7738d50a4f4f
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2019-04-05 11:42:21 -0300

    Change line ending to LF (Unix)

--- Java/problem_1/Algorithm.java
+++ Java/problem_1/Algorithm.java
@@ -5,7 +5,7 @@ class Algorithm {
 
         for(int i = 0; i < 1000; ++i) {
             if ((i % 3 == 0) || (i % 5 == 0)) {
-                sum += i;  
+                sum += i;
             }
         }
 
