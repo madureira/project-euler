006d366d229bc58b1e2d3883bb49870680b32280
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2019-04-05 22:28:07 -0300

    Add Javascript problems solutions

---
+++ Javascript/problem_1/algorithm.js
@@ -0,0 +1,13 @@
+function main() {
+  let sum = 0;
+
+  for(let i = 0; i < 1000; ++i) {
+    if ((i % 3 === 0) || (i % 5 === 0)) {
+      sum += i;
+    }
+  }
+
+  console.log(sum);
+}
+
+main();
---
+++ Javascript/problem_2/algorithm.js
@@ -0,0 +1,21 @@
+function evenFib(previous, term, sum) {
+  let total = previous + term;
+
+  if (total > 4000000) {
+    return sum;
+  }
+
+  if (total % 2 == 0) {
+    sum += total;
+  }
+
+  return evenFib(term, total, sum);
+}
+
+function main() {
+  const sum = evenFib(1, 1, 0);
+
+  console.log(sum);
+}
+
+main();
