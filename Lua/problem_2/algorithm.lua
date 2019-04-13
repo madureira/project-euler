2d81c46edf0a11c1e3df511b5f5271cae562f4c4
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2019-04-13 16:42:56 -0300

    Fix indentation

--- Lua/problem_2/algorithm.lua
+++ Lua/problem_2/algorithm.lua
@@ -2,20 +2,20 @@ local PHI = (1 + math.sqrt(5)) / 2;
 local PSI = (1 - math.sqrt(5)) / 2;
 
 function reverseFib(fn)
-    return math.floor(math.log((fn * math.sqrt(5) + math.sqrt(5 * math.pow(fn, 2) - 4)) / 2) / math.log(PHI));
+  return math.floor(math.log((fn * math.sqrt(5) + math.sqrt(5 * math.pow(fn, 2) - 4)) / 2) / math.log(PHI));
 end
 
 function getK(n)
-    return math.floor(reverseFib(n) / 3);
+  return math.floor(reverseFib(n) / 3);
 end
 
 function sumEven(k)
-    local phi3 = math.pow(PHI, 3);
-    local psi3 = math.pow(PSI, 3);
+  local phi3 = math.pow(PHI, 3);
+  local psi3 = math.pow(PSI, 3);
 
-    return math.floor((1 / math.sqrt(5)) * (
-        phi3 * ((1 - math.pow(phi3, k)) / (1 - phi3)) - psi3 * ((1 - math.pow(psi3, k)) / (1 - psi3))
-    ));
+  return math.floor((1 / math.sqrt(5)) * (
+    phi3 * ((1 - math.pow(phi3, k)) / (1 - phi3)) - psi3 * ((1 - math.pow(psi3, k)) / (1 - psi3))
+  ));
 end
 
 function main()
