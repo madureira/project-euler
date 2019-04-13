1436bb939eec71be1ec6d736b2a69e969f257456
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2019-04-13 15:34:16 -0300

    Refactor Java's problem 2

--- Java/problem_2/Algorithm.java
+++ Java/problem_2/Algorithm.java
@@ -1,23 +1,36 @@
+import static java.lang.Math.sqrt;
+import static java.lang.Math.floor;
+import static java.lang.Math.log;
+import static java.lang.Math.pow;
+
 class Algorithm {
+  
+    private static final double PHI = (1 + sqrt(5)) / 2;
+    private static final double PSI = (1 - sqrt(5)) / 2;
 
-    public static int evenFib(int previous, int term, int sum) {
-        int total = previous + term;
+    private static int reverseFib(int fn) {
+        return (int) floor(log((fn * sqrt(5) + sqrt(5 * pow(fn, 2) - 4)) / 2) / log(PHI));
+    }
 
-        if (total > 4000000) {
-            return sum;
-        }
+    private static int getK(int n) {
+        return (int) floor(reverseFib(n) / 3);
+    }
 
-        if (total % 2 == 0) {
-            sum += total;
-        }
+    private static int sumEven(int k) {
+        double phi3 = pow(PHI, 3);
+        double psi3 = pow(PSI, 3);
 
-        return evenFib(term, total, sum);
+        return (int) ((1 / sqrt(5)) * (
+            phi3 * ((1 - pow(phi3, k)) / (1 - phi3)) - psi3 * ((1 - pow(psi3, k)) / (1 - psi3))
+        ));
     }
 
     public static void main(String[] args) {
-        int sum = evenFib(1, 1, 0);
+        int N = 4000000;
+        int k = getK(N);
+        int S = sumEven(k);
 
-        System.out.println(sum);
+        System.out.println(S);
     }
 
 }
